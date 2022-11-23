baa55842cca48c5991b3a1daca58304a21a5af0f
Kosuke Yamagishi <kosuke1012@gmail.com>
2022-11-24 01:25:01 +0900

    refactor

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,61 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
+
+	"github.com/goccy/go-yaml"
 
 	copl "github.com/kosuke1012/CoPL/CoPL"
 )
 
+var noFlag = flag.Int("n", 0, "Question no to answer.")
+
+type workbook struct {
+	Workbook []answer
+}
+type answer struct {
+	No      string
+	Game    string
+	Anspath string
+}
+
 func main() {
+	flag.Parse()
+
+	var w workbook
+	err := w.readWorkbook()
+	if err != nil {
+		panic(err)
+	}
+	var q answer
+	for _, work := range w.Workbook {
+		if work.No == strconv.Itoa(*noFlag) {
+			q = work
+			break
+		}
+	}
+	if q.No == "" {
+		panic("Question no not found.")
+	}
+	f, err := os.Open(q.Anspath)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	b, err := io.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	ans := string(b)
 	loginName := os.Getenv("loginName")
-	ans := `
-	S(S(Z)) is less than S(S(S(Z))) by L-Succ{}
-	`
 	res, err := postAnswer(loginName, "9", "CompareNat1", ans)
 	if err != nil {
 		panic(err)
@@ -27,6 +67,23 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(resStr)
+}
+
+func (w *workbook) readWorkbook() error {
+	f, err := os.Open("../workbook.yaml")
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+	b, err := io.ReadAll(f)
+	if err != nil {
+		return err
+	}
+
+	if err := yaml.Unmarshal(b, w); err != nil {
+		return err
+	}
+	return nil
 }
 
 func postAnswer(lname, q, game, ans string) (*http.Response, error) {
--- go.mod
+++ go.mod
@@ -3,12 +3,18 @@ module github.com/kosuke1012/CoPL
 go 1.19
 
 require (
+	github.com/goccy/go-yaml v1.9.6
 	github.com/stretchr/testify v1.8.1
 	golang.org/x/net v0.2.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
