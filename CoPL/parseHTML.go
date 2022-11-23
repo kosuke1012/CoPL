7ff3db3748aea659a46321e1e611f0c0c67fc9e3
Kosuke Yamagishi <kosuke1012@gmail.com>
2022-11-23 11:44:51 +0900

    refactor parse HTML

--- CoPL/parseHTML.go
+++ CoPL/parseHTML.go
@@ -3,6 +3,7 @@ package CoPL
 import (
 	"fmt"
 	"io"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -15,29 +16,33 @@ func ParseHTMLResult(r io.Reader) (string, error) {
 		case html.ErrorToken:
 			return "", fmt.Errorf("div main id not found.")
 		case html.StartTagToken:
-			if _, hasAttr := t.TagName(); hasAttr {
-				if key, val, _ := t.TagAttr(); string(key) == "id" && string(val) == "main" {
-					data, err := findH1Tag(t)
-					if err != nil {
-						panic(err)
-					}
-					return data, nil
-				}
+			if _, hasAttr := t.TagName(); !hasAttr {
+				continue
 			}
-			continue
+			if key, val, _ := t.TagAttr(); string(key) == "id" && string(val) == "main" {
+				var b strings.Builder
+				for _, s := range []string{"h1", "pre"} {
+					data, err := findStartTag(t, s)
+					if err != nil {
+						return "", err
+					}
+					fmt.Fprintln(&b, data)
+				}
+				return b.String(), nil
+			}
 		default:
 			continue
 		}
 	}
 }
 
-func findH1Tag(t *html.Tokenizer) (string, error) {
+func findStartTag(t *html.Tokenizer, tagName string) (string, error) {
 	for {
 		switch token := t.Token(); token.Type {
 		case html.ErrorToken:
-			return "", fmt.Errorf("h1 tag not found.")
+			return "", fmt.Errorf("%s tag not found.", tagName)
 		case html.StartTagToken:
-			if token.Data == "h1" {
+			if token.Data == tagName {
 				t.Next()
 				return t.Token().Data, nil
 			}
